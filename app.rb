8df37e036e858fcb6625ed91b13d7918e56c4914
Chad Palmer <chad.palmer@yale.edu>
2020-04-09 14:35:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 name = "Chad"
 age = 20
 favorite_food = "chocolate cake"
-height_in_inches = "74"
-height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
+height_in_inches = 74
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average." : "You are not taller than average."
 
 output(name, age, favorite_food, height_message)
